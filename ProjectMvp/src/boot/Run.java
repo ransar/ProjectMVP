c9c1e47ae95f8cf886c5245943ae152964d014b3
itzikg211 <itzikg211@gmail.com>
2015-05-18 20:15:06 +0300

    fixed errors

--- ProjectMvp/src/boot/Run.java
+++ ProjectMvp/src/boot/Run.java
@@ -23,7 +23,7 @@ public class Run {
 		//my Program
 		//System.out.println("hello");
 		MazeSolutionHibernate ms = new MazeSolutionHibernate();
-		MazeGenerator mg=new DFSMazeGenerator();
+		MazeGenerator mg=(MazeGenerator) new DFSMazeGenerator();
 		Maze maze = mg.generateMaze(8,8);
 		ms.setMaze(maze.toString());
 		//Maze temp = ms.stringToMaze(maze.toString());
